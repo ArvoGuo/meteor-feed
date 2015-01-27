ea60b6f8765ee25dedbc691ba2b564b8c029ddbc
xiongyj <xiongyj@knownsec.com>
2015-01-27 13:50:38 +0800

    添加初始化数据

---
+++ feed/server/init.js
@@ -0,0 +1,33 @@
+//初始化管理员和部门
+Meteor.startup(function(){
+  if (Department.find().count() === 0){
+    Department.insert({
+      name:'研发部'
+    });
+    Department.insert({
+      name:'运营部'
+    });
+    Department.insert({
+      name:'人事行政部'
+    });
+  }
+
+  if(Meteor.users.find().count() === 0){
+    Accounts.createUser({
+      email:'admin@qq.com',
+      password:'123456',
+      profile:{
+        name:"admin",
+        department: '研发部',
+        email:"admin@qq.com"
+      }
+    });
+    Users.update({
+      roles: ['user']
+    },{
+      '$set':{
+        roles: ['admin'],
+      }
+    });
+  }
+});
